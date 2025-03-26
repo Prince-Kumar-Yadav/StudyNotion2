10d2583b818246c6a5e74eb8a79f3d121244271d
Prince Kumar Yadav <pyadav476@rku.ac.in>
2025-03-26 21:30:02 +0530

    Add the forget Passweord Page and some change on Home Page

--- obj/Release/net8.0/Study_Notion.AssemblyInfo.cs
+++ obj/Release/net8.0/Study_Notion.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Study_Notion")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8720fff94aafa60351c980abeab1007dff485901")]
 [assembly: System.Reflection.AssemblyProductAttribute("Study_Notion")]
