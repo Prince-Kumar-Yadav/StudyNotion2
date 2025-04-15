cd5e9a4bfc4c640344fe3a90b31585c2d448078d
Prince Kumar Yadav <pyadav476@rku.ac.in>
2025-04-15 11:19:42 +0530

    Some Changes

--- obj/Debug/net8.0/Study_Notion.AssemblyInfo.cs
+++ obj/Debug/net8.0/Study_Notion.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Study_Notion")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8720fff94aafa60351c980abeab1007dff485901")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10d2583b818246c6a5e74eb8a79f3d121244271d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Study_Notion")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Study_Notion")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
